ac4d4e09b2f4618704c6c635dc3567daf78469b7
ElvisHuang <63724830@qq.com>
2017-10-17 16:41:39 +0800

    version:1.0.0 1. add Message

---
+++ GjBaseLibrary/src/main/java/tv/guojiang/baselib/message/IMessageHelper.java
@@ -0,0 +1,24 @@
+package com.efeizao.massage;
+
+/**
+ * author  KK
+ * date 2017/8/17
+ */
+
+public interface IMessageHelper {
+
+    public void addMessageRecievedListener(String messageType, MessageReceivedListener ls);
+
+    public void removeMessageRecievedListener(String messageType, MessageReceivedListener ls);
+
+    public void addMessageRecievedListener(String[] messageTypes, MessageReceivedListener ls);
+
+    public void removeMessageRecievedListener(String[] messageTypes, MessageReceivedListener ls);
+
+    public void post(MessageInfo info);
+
+//    public static interface MessageReceivedListener {
+//        void onMessageRecieved(MessageInfo message);
+//
+//    }
+}
---
+++ GjBaseLibrary/src/main/java/tv/guojiang/baselib/message/MessageInfo.java
@@ -0,0 +1,26 @@
+package com.efeizao.massage;
+
+/**
+ * 消息实体
+ * author  KK
+ * date 2017/8/17
+ */
+public class MessageInfo {
+
+    public String messageType;
+    public String msgText;//json
+    public Object object;
+//    public int errno;
+//    public String msg;
+
+    public MessageInfo(String messageType, String msgText, Object object) {
+        this.messageType = messageType;
+        this.msgText = msgText;
+        this.object = object;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
