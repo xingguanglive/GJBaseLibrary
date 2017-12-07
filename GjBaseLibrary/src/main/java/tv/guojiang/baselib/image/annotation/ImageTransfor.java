e11f58e8b3c16ce9eb9a65c4357cd15044e7611c
Elvis <63724830@qq.com>
2017-12-07 10:43:16 +0800

    version:1.2.0 1. imageTransfor 提交遗漏修复

--- GjBaseLibrary/src/main/java/tv/guojiang/baselib/image/annotation/ImageTransfor.java
+++ GjBaseLibrary/src/main/java/tv/guojiang/baselib/image/annotation/ImageTransfor.java
@@ -14,7 +14,10 @@ import tv.guojiang.baselib.image.model.ImageConstants;
  */
 
 @IntDef({ImageConstants.IMAGE_TRANSFOR_DEFAULT, ImageConstants.IMAGE_TRANSFOR_CROP_CIRCLE,
-		ImageConstants.IMAGE_TRANSFOR_CROP_CORNER, ImageConstants.IMAGE_TRANSFOR_BLUR})
+		ImageConstants.IMAGE_TRANSFOR_CROP, ImageConstants.IMAGE_TRANSFOR_CROP_SQUARE,
+		ImageConstants.IMAGE_TRANSFOR_CROP_CORNER, ImageConstants.IMAGE_TRANSFOR_COLOR_FILTER,
+		ImageConstants.IMAGE_TRANSFOR_GRAY_SCALE, ImageConstants.IMAGE_TRANSFOR_BLUR,
+		ImageConstants.IMAGE_TRANSFOR_MASK})
 @Retention(RetentionPolicy.SOURCE)
 public @interface ImageTransfor {
 }
