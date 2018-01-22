b06c0dddbd0a0a0ca85021bb7d31eb5bedebb2c4
chentao7v <chentao7v@gmail.com>
2018-01-22 18:18:55 +0800

    release

--- app/src/main/java/tv/guojiang/sample/NetworkSampleActivity.java
+++ app/src/main/java/tv/guojiang/sample/NetworkSampleActivity.java
@@ -51,7 +51,7 @@ public class NetworkSampleActivity extends AppCompatActivity {
         // 初始化全局接口通用配置
         ApiClient apiClient = new Builder(this)
             .baseUrl("http://www.baidu.com/")
-            .log(true)
+            .log(false)
             .cookie(true)
             .joinParamsIntoUrl(false)
             //            .mockData(true) // 模拟数据会直接跳过外网的访问，直接成功
