aad4bba8f92416b97acaaf36e1bd129439559396
chentao7v <chentao7v@gmail.com>
2018-05-23 16:49:27 +0800

    删除网络框架

--- sample/src/main/java/tv/guojiang/MainActivity.java
+++ sample/src/main/java/tv/guojiang/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import tv.guojiang.base.R;
 import tv.guojiang.sample.ImageLoadSampleActivity;
 import tv.guojiang.sample.LogSampleActivity;
-import tv.guojiang.sample.NetworkSampleActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void imageLoader(View view) {
         Intent intent = new Intent(this, ImageLoadSampleActivity.class);
-        startActivity(intent);
-    }
-
-    public void network(View view) {
-        Intent intent = new Intent(this, NetworkSampleActivity.class);
         startActivity(intent);
     }
 
